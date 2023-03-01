cbf681288886062080d6fca6c62c8c7840156cb0
Konrad Kovacs <konrad.kovacs97@outlook.com>
2023-03-01 19:34:59 +0100

    additional input for better search

--- PartFinder.py
+++ PartFinder.py
@@ -24,31 +24,37 @@ def get_parts(serial_number):
         return sorted(parts, key=lambda x: x["price"])
 
 serial_number = input("Enter the serial number of the part: ")
-brand = input("Enter the brand of your vehicle: ")
-vehicle_type = input("Enter the type of your vehicle: ")
-i=0
+additional_info = input("Do you want to share more information about your car?(If you share it will recommend the best option) Skip if no")
+if (additional_info == "yes"):
+    brand = input("Enter the brand of your vehicle: ")
+    vehicle_type = input("Enter the type of your vehicle: ")
 
-parts = get_parts(serial_number)
-print("\n")
+    parts = get_parts(serial_number)
+    print("\n")
 
-if not parts:
-    print("There are no parts with this serial number")
-else:
-    # Filter parts by brand and vehicle type
-    matching_parts = [part for part in parts if brand.lower() in part['description'].lower() and vehicle_type.lower() in part['description'].lower()]
-
-    if not matching_parts:
-        print("There are no parts that match your vehicle")
-
+    if not parts:
+        print("There are no parts with this serial number")
     else:
-        # Recommend the part with the lowest price
-        recommended_part = min(matching_parts, key=lambda x: x['price'])
+        # Filter parts by brand and vehicle type
+        matching_parts = [part for part in parts if brand.lower() in part['description'].lower() and vehicle_type.lower() in part['description'].lower()]
 
-        print(f"The recommended part for your {brand} {vehicle_type} is:")
-        print(f"{recommended_part['name']} - {recommended_part['price']} - and it was in {recommended_part['description']} - LINK: {recommended_part['link']}")
-        print("\n")
+        if not matching_parts:
+            print("There are no parts that match your vehicle")
 
-        print(f"All Parts for serial number {serial_number}:")
-        for part in parts:
-            print(f"{part['name']} - for {part['price']} - and it was in {part['description']} - LINK: {part['link']}")
-        print("\n")
+        else:
+            # Recommend the part with the lowest price
+            recommended_part = min(matching_parts, key=lambda x: x['price'])
+
+            print(f"The recommended part for your {brand} {vehicle_type} is:")
+            print(f"{recommended_part['name']} - {recommended_part['price']} - and it was in {recommended_part['description']} - LINK: {recommended_part['link']}")
+            print("\n")
+
+            print(f"All Parts for serial number {serial_number}:")
+            for part in parts:
+                print(f"{part['name']} - for {part['price']} - and it was in {part['description']} - LINK: {part['link']}")
+            print("\n")
+else:
+    parts = get_parts(serial_number)
+    print("\n")
+    for part in parts:
+        print(f"{part['name']} - {part['price']} - and it was in {part['description']} - LINK: {part['link']}")
