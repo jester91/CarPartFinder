385ea9506fdf5dfb9a2c8b54004ec8af19c52fc0
Konrad Kovacs <konrad.kovacs97@outlook.com>
2023-03-21 23:12:11 +0100

    implelent olx webscrape

--- PartFinder.py
+++ PartFinder.py
@@ -2,34 +2,54 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-def get_parts(serial_number):
+def get_parts_ovoko(serial_number):
     options = Options()
     options.headless = True
-    driver = webdriver.Chrome(options=options)
-    url = f"https://ovoko.pl/szukaj?q={serial_number}"
-    print(f"URL: {url}")
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    parts = []
-    for item in soup.find_all("div", class_="products__items"):
-        name = item.find("div", class_="products__text__header")
-        price = item.find("div", class_="products__price")
-        description = item.find("div", class_="products__text__description__main")
-        link = item.find("a", href=True, class_="ga-tracking-event-js")
-        parts.append({"name": name.text.strip(), "description": description.text.strip(), "price": price.text.strip(), "link": link['href']})
-    driver.quit()
-    if not parts:
-        print("There are no parts with this serial number")
-    else:
-        return sorted(parts, key=lambda x: x["price"])
+    with webdriver.Chrome(options=options) as driver:
+        url = f"https://ovoko.pl/szukaj?q={serial_number}"
+        print(f"URL: {url}")
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        parts = []
+        for item in soup.find_all("div", class_="products__items"):
+            name = item.find("div", class_="products__text__header")
+            price = item.find("div", class_="products__price")
+            description = item.find("div", class_="products__text__description__main")
+            link = item.find("a", href=True, class_="ga-tracking-event-js")
+            parts.append({"name": name.text.strip(), "description": description.text.strip(), "price": price.text.strip(), "link": link['href']})
+        if not parts:
+            print("There are no parts with this serial number")
+        else:
+            return sorted(parts, key=lambda x: x["price"])
+
+def get_parts_olx(serial_number):
+    options = Options()
+    options.headless = True
+    with webdriver.Chrome(options=options) as driver:
+        url = f"https://www.olx.pl/oferty/q-{serial_number}/"
+        print(f"URL: {url}")
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        olx_parts = []
+        for item in soup.find_all("div", class_="css-oukcj3"):
+            name = item.find("h6", class_="css-16v5mdi er34gjf0")
+            price = item.find("p", class_="css-10b0gli er34gjf0")
+            description = item.find("p", class_="css-veheph er34gjf0")
+            link = item.find("a", href=True, class_="css-rc5s2u")
+            olx_parts.append({"name": name.text.strip(),"description": description.text.strip(), "price": price.text.strip(), "link": link['href']})
+        if not olx_parts:
+            print("There are no parts with this serial number")
+        else:
+            return sorted(olx_parts, key=lambda x: x["price"])
+
 
 serial_number = input("Enter the serial number of the part: ")
-additional_info = input("Do you want to share more information about your car?(If you share it will recommend the best option) Skip if no")
+additional_info = input("Do you want to share more information about your car? (If you share it will recommend the best option) Skip if no ")
 if (additional_info == "yes"):
     brand = input("Enter the brand of your vehicle: ")
     vehicle_type = input("Enter the type of your vehicle: ")
 
-    parts = get_parts(serial_number)
+    parts = get_parts_ovoko(serial_number)
     print("\n")
 
     if not parts:
@@ -54,7 +74,9 @@ if (additional_info == "yes"):
                 print(f"{part['name']} - for {part['price']} - and it was in {part['description']} - LINK: {part['link']}")
             print("\n")
 else:
-    parts = get_parts(serial_number)
+#TODO I have to find a better solution to look up different websites
+    parts = get_parts_ovoko(serial_number)
+    parts+= get_parts_olx(serial_number)
     print("\n")
     for part in parts:
         print(f"{part['name']} - {part['price']} - and it was in {part['description']} - LINK: {part['link']}")
